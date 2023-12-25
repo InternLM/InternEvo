3361350348801318cac32b7c81fdabe0347792a2
huangting4201 <1538303371@qq.com>
2023-12-25 12:02:23 +0800

    fix(parallel_context.py): fix seed mode when TENSOR parallel

--- internlm/core/context/parallel_context.py
+++ internlm/core/context/parallel_context.py
@@ -599,8 +599,10 @@ class ParallelContext(metaclass=SingletonMeta):
         # device of the same data parallel group. The underlying reason is that the device of tp_rank = 0 will perform
         # additional random operations during the RowParallelLinear module building process.
         # set_mode(ParallelMode.DUMMY)
-        set_mode(ParallelMode.TENSOR)
-        set_mode(ParallelMode.WEIGHT)
+        if self.is_using_parallel_mode(ParallelMode.TENSOR):
+            set_mode(ParallelMode.TENSOR)
+        if self.is_using_parallel_mode(ParallelMode.WEIGHT):
+            set_mode(ParallelMode.WEIGHT)
 
         seeds = get_seeds()
         seed_str = ", ".join([f"{k}: {v}" for k, v in seeds.items()])
