Fix(param overlap): fix overlap of broadcasting and computing (#46)

internlm/solver/optimizer/hybrid_zero_optim.py
@@ -112,8 +112,6 @@ class HybridZeroOptimizer(BaseOptimizer):
         # self._overlap_communication = overlap_communication
         self._reduce_bucket_size = reduce_bucket_size
 
-        self._comm_bcast_stream = torch.cuda.Stream()
-
         # gradient scaler
         self.grad_scaler = DynamicGradScaler(
             initial_scale=initial_scale,
@@ -1042,8 +1040,7 @@ class HybridZeroOptimizer(BaseOptimizer):
                     fp32_param = self._fp32_flat_param_groups_of_current_rank[group_id]
                     fp16_param.data.copy_(fp32_param)
         torch.cuda.synchronize()
-        with torch.cuda.stream(self._comm_bcast_stream):
-            self.broadcast_params()
+        self.broadcast_params()
 
         timer("step").stop()
 
@@ -1079,8 +1076,6 @@ class HybridZeroOptimizer(BaseOptimizer):
 
         for handle in handles:
             handle.wait()
-
-        torch.cuda.synchronize()
 
     ##################
     # FP16 Utilities #