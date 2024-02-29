Fix/fix broadcast overlap with isp (#64)

internlm/core/communication/isp.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 from functools import partial
-from typing import Any, Dict, List, Union
+from typing import Any, Callable, Dict, List, Union
 
 import torch
 from torch import distributed as dist
@@ -135,7 +135,8 @@ class ISPOverlapState:
         self.last_ckpt_block: nn.Module = None
         self.isp_outs: List[nn.Module] = []
         self.isp_modules: List[nn.Module] = []
-        self.index_to_isp_module: Dict[int, nn.Module] = {}
+        self.index_to_isp_modules: Dict[int, nn.Module] = {}
+        self.index_to_block: Dict[int, nn.Module] = {}
         self.module_to_index: Dict[nn.Module, int] = {}
         self.weight_global_handle: Dict[str, Any] = {}
         self.weight_global_output: Dict[str, torch.Tensor] = {}
@@ -163,6 +164,7 @@ class ISPCommunicator:
         self.is_forward = True
         self.reduce_scatter_handlers = {}
         self._module_shapes = {}
+        self._forward_prefetch_prerequisites = []
 
         # real overlap state for each chunk.
         self._overlap_states: Dict[int, ISPOverlapState] = {}
@@ -186,7 +188,9 @@ class ISPCommunicator:
         # key: isp module; value: transformer block index
         self._module_to_index = None
         # key: transformer block index; value: isp modules
-        self._index_to_isp_module = None
+        self._index_to_isp_modules = None
+        # key: transformer block index; value: transformer block
+        self._index_to_block = None
 
         # init overlap states if necessary.
         if self.overlap:
@@ -228,7 +232,8 @@ class ISPCommunicator:
                 ]
 
                 for idx, block in enumerate(children):
-                    self._overlap_states[cid].index_to_isp_module[idx] = []
+                    self._overlap_states[cid].index_to_isp_modules[idx] = []
+                    self._overlap_states[cid].index_to_block[idx] = block
                     for sub_name, sub in block.named_children():
                         for name, child in sub.named_children():
                             if name in ["out_proj", "wo"]:
@@ -243,7 +248,7 @@ class ISPCommunicator:
                                     self._module_shapes[name] = torch.Size(origin_shape)
                                 self._overlap_states[cid].module_to_index[child] = idx
                                 self._overlap_states[cid].isp_modules.append(child)
-                                self._overlap_states[cid].index_to_isp_module[idx].append(child)
+                                self._overlap_states[cid].index_to_isp_modules[idx].append(child)
 
                                 setattr(child, "isp_name", name)
 
@@ -260,7 +265,7 @@ class ISPCommunicator:
                                         f"{full_name}.bias",
                                     )
 
-        self._overlap_states[cid].num_blocks = len(self._overlap_states[cid].index_to_isp_module)
+        self._overlap_states[cid].num_blocks = len(self._overlap_states[cid].index_to_isp_modules)
 
     def _all_gather_module_weight(self, module):
         with_bias = module.bias is not None
@@ -307,7 +312,15 @@ class ISPCommunicator:
         self._weight_global_output[module] = weight_output
 
     def _all_gather_block_weight(self, block_index: int):
-        for module in self._index_to_isp_module[block_index]:
+        block = self._index_to_block[block_index]
+
+        # wait for prerequisite conditions
+        if self.is_forward:
+            for callback in self._forward_prefetch_prerequisites:
+                callback(block)
+
+        # prefetch parameters for all isp modules of the block
+        for module in self._index_to_isp_modules[block_index]:
             self._all_gather_module_weight(module)
 
     def _wait_handle(self, module):
@@ -358,7 +371,7 @@ class ISPCommunicator:
         self._wait_handle(module)
 
     def _pre_forward_hook_for_block(self, *args):  # pylint: disable=W0613
-        for module in self._index_to_isp_module[self._ckpt_block_num - 1]:
+        for module in self._index_to_isp_modules[self._ckpt_block_num - 1]:
             self._all_gather_module_weight(module)
 
     def _post_forward_hook_for_module(self, module: nn.Module, *args):  # pylint: disable=W0613
@@ -446,12 +459,40 @@ class ISPCommunicator:
         self._weight_global_output = self._overlap_states[chunk_id].weight_global_output
         self._bias_global_output = self._overlap_states[chunk_id].bias_global_output
         self._module_to_index = self._overlap_states[chunk_id].module_to_index
-        self._index_to_isp_module = self._overlap_states[chunk_id].index_to_isp_module
+        self._index_to_isp_modules = self._overlap_states[chunk_id].index_to_isp_modules
+        self._index_to_block = self._overlap_states[chunk_id].index_to_block
         self._ckpt_block_num = self._overlap_states[chunk_id].ckpt_block_num
         self._last_ckpt_block = self._overlap_states[chunk_id].last_ckpt_block
         self._head = self._overlap_states[chunk_id].head
         self._embedding = self._overlap_states[chunk_id].embedding
         self._num_blocks = self._overlap_states[chunk_id].num_blocks
+
+    def register_prerequisite_for_forward_prefetch_hooks(self, prerequisite_func: Callable) -> None:
+        """
+        Registers a callback function that specifies a prerequisite condition for
+        prefetching parameters before forward computation.
+
+        This method allows users to define custom logic that must be satisfied before
+        parameters are fetched for the next forward pass. This can be useful for
+        implementing complex parameter update strategies or for coordinating
+        parameter access with other system components.
+
+        Args:
+            prerequisite_func (Callable): A callable that represents the prerequisite
+                                    condition. This function will be invoked before
+                                    the parameters are prefetched, and its return value
+                                    will determine whether the prefetching should proceed.
+
+        Returns:
+            None: This method does not return any value.
+
+        Raises:
+            TypeError: If the provided 'prerequisite_func' is not callable.
+        """
+        if not callable(prerequisite_func):
+            raise TypeError("The provided prerequisite function must be callable.")
+
+        self._forward_prefetch_prerequisites.append(prerequisite_func)
 
     # communication operation interfaces
 
@@ -521,8 +562,7 @@ class ISPCommunicatorSchedulerHook(SchedulerHook):
         self._zero_optim = zero_optim
 
     def before_forward(self, scheduler, inputs) -> None:
-        if self._isp_communicator._ckpt_block_num > 0:
-            self._isp_communicator.is_forward = True
+        self._isp_communicator.is_forward = True
         # switch model chunk before forward
         chunk_id = 0 if gpc.virtual_pipeline_parallel_rank is None else gpc.virtual_pipeline_parallel_rank
         self._isp_communicator.switch_current_model_chunk(chunk_id)
@@ -537,8 +577,7 @@ class ISPCommunicatorSchedulerHook(SchedulerHook):
         pass
 
     def before_backward(self, scheduler, outputs, outputs_grad) -> None:
-        if self._isp_communicator._ckpt_block_num > 0:
-            self._isp_communicator.is_forward = False
+        self._isp_communicator.is_forward = False
         # switch model chunk before backward
         chunk_id = 0 if gpc.virtual_pipeline_parallel_rank is None else gpc.virtual_pipeline_parallel_rank
         self._isp_communicator.switch_current_model_chunk(chunk_id)

internlm/solver/optimizer/utils.py
@@ -3,18 +3,15 @@
 
 import math
 from abc import ABC, abstractmethod
-from collections import OrderedDict
-from functools import partial
-from typing import Any, Dict, Optional, Union
+from typing import Dict, Optional
 
 import torch
 import torch.distributed as dist
-from torch import Tensor, nn
+from torch import Tensor
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 
 from internlm.core.context import ParallelMode
 from internlm.core.context import global_context as gpc
-from internlm.core.naive_amp import NaiveAMPModel
 from internlm.utils.common import get_current_device, get_tensor_norm, move_norm_to_cuda
 from internlm.utils.logger import get_logger
 from internlm.utils.parallel import (
@@ -861,90 +858,3 @@ class DynamicGradScaler(BaseGradScaler):
         self._scale = self._scale.fill_(state_dict["_scale"])
         self._growth_step = state_dict["_growth_step"]
         self._hysteresis_step = state_dict["_hysteresis_step"]
-
-
-class ParamBcastSyncHandler:
-    """
-    Model Partition Handler for overlap broadcast with forward
-    """
-
-    def __init__(self, model: Union[nn.Module, nn.ModuleList]) -> None:
-        self._block_to_param = OrderedDict()  # <key: nn.Module> <value: list(param)>
-        self._param_to_rank = dict()  # <key: param> <value: rank)>
-        self._block_to_rank = dict()  # <key: nn.Module> <value: rank)>
-        self._bcast_handles = dict()  # <key: rank> <value: list(bcast handles))>
-
-        zero1_size = gpc.get_world_size(ParallelMode.ZERO1)
-        total_param_num = sum(p.numel() for p in model.parameters())
-        avg_param_num = total_param_num * 1.0 // zero1_size
-
-        # just want to share same for loop for ModuleList and Module
-        if not isinstance(model, nn.ModuleList):
-            model = [model]
-
-        # record the parameters to transformer/embeding/head/norm block
-        for _chunk in model:
-            if isinstance(_chunk, NaiveAMPModel):
-                _chunk = _chunk.model
-
-            for _, children in _chunk.named_children():
-                # should be the transformer block definaton in modeling_xxx.py
-                if isinstance(children, nn.ModuleList):
-                    # record the block that a parameter belongs to
-                    for _, block in enumerate(children):
-                        # self._block_to_param[f"{name}.{idx}"] = list(block.parameters())
-                        self._block_to_param[block] = list(block.parameters())
-                else:
-                    # record the block that a parameter belongs to
-                    # self._block_to_param[name] = list(children.parameters())
-                    self._block_to_param[children] = list(children.parameters())
-
-        alloc_num = 0
-        rank_to_go = 0
-
-        # process the parameters in block_to_param sequencially,
-        # allocate each parameter to a local rank of ParallelMode.ZERO1,
-        # NOTE that we do NOT consider following scenarios:
-        # 1) whether a parameter is trainable;
-        # 2) paramters maybe in different optimizer group
-        for block, params in self._block_to_param.items():
-            # allocate a model block to a local rank of ParallelMode.ZERO1
-            self._block_to_rank[block] = [rank_to_go]
-            for p in params:
-                alloc_num = alloc_num + p.numel()
-                # in this case, allocate the param to next rank if possible
-                if alloc_num > avg_param_num * 1.01 and rank_to_go < zero1_size - 1:
-                    rank_to_go = rank_to_go + 1
-                    alloc_num = 0
-                    self._block_to_rank[block].append(rank_to_go)
-                # allocate a parameter to a local rank of ParallelMode.ZERO1
-                self._param_to_rank[p] = rank_to_go
-
-        # initialize an empty list for _bcast_handles of each rank
-        for rank in range(gpc.get_world_size(ParallelMode.ZERO1)):
-            self._bcast_handles[rank] = []
-
-        # register_forward_pre_hook for transformer/embeding/norm/xxx block
-        self._register_sync_parameters_hook()
-
-    def _register_sync_parameters_hook(self) -> None:
-        def _pre_forward_hook(model: nn.Module, inputs: Any):  # pylint: disable=W0613
-            bcast_handles = []
-            # gather all required broadcast hanles into a list
-            for rank in self._block_to_rank[model]:
-                bcast_handles.extend(self._bcast_handles[rank])
-                # need to clear _bcast_handles since they would be processed later
-                self._bcast_handles[rank] = []
-            # wait all required broadcast handles to be completed
-            for handle in bcast_handles:
-                handle.wait()
-
-        # register_forward_pre_hook for transformer/embeding/norm/xxx block
-        for block, _ in self._block_to_rank.items():
-            block.register_forward_pre_hook(partial(_pre_forward_hook))
-
-    def get_rank_by_param(self, param) -> int:
-        return self._param_to_rank[param]
-
-    def add_bcast_handle(self, rank, handle) -> None:
-        self._bcast_handles[rank].append(handle)