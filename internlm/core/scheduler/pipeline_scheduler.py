42830b2204ca764987ee57af897703f103e55878
Sun Peng <sunpengsdu@gmail.com>
2024-03-29 11:34:59 +0800

    fix(unpack_data): pad -100 on labels

--- internlm/core/scheduler/pipeline_scheduler.py
+++ internlm/core/scheduler/pipeline_scheduler.py
@@ -824,7 +824,9 @@ class InterleavedPipelineScheduler(PipelineScheduler):
             micro_batch_data["input_ids"] = self.data_process_func(
                 micro_batch_data["input_ids"], micro_batch_data["cu_seqlens"]
             )
-            micro_batch_label = self.data_process_func(micro_batch_label, micro_batch_data["cu_seqlens"])
+            micro_batch_label = self.data_process_func(
+                micro_batch_label, micro_batch_data["cu_seqlens"], padding_v=-100
+            )
 
             micro_batch_data.pop("cu_seqlens")
             micro_batch_data.pop("indexes")
