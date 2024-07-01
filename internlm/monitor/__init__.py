aa79f69ce5462303d6c776ec9550e40496f0c6e2
zigzagcai <caizheng@pjlab.org.cn>
2024-07-01 17:15:18 +0800

    fix pylint

--- internlm/monitor/__init__.py
+++ internlm/monitor/__init__.py
@@ -1,6 +1,6 @@
+from .internevo_monitor import internevo_monitor
 from .monitor import initialize_monitor_manager, send_alert_message
 from .utils import set_env_var
-from .internevo_monitor import internevo_monitor
 
 __all__ = [
     "send_alert_message",
