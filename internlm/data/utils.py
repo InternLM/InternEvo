28e0da51084c32fc7f972f17170034febbf4ca11
zigzagcai <caizheng@pjlab.org.cn>
2024-08-05 15:58:51 +0800

    fix merge conflicts

--- internlm/data/utils.py
+++ internlm/data/utils.py
@@ -52,9 +52,6 @@ def unpack_type_ids(type_ids, cu_seqlens):
 
 def unpack_data(data, label):
 
-    if gpc.config.model_type == ModelType.HF.name:
-        return data, label
-
     data["input_ids"] = _unpack_data(data["input_ids"], data["cu_seqlens"], padding_v=0).squeeze(0)
     label = _unpack_data(label, data["cu_seqlens"], padding_v=-100).squeeze(0)
 
