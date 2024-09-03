ad22aa7550383d959a8c528dbfe33b3541e151e9
zigzagcai <caizheng@pjlab.org.cn>
2024-09-03 17:58:24 +0800

    small fix

--- internlm/core/parallel/shard.py
+++ internlm/core/parallel/shard.py
@@ -31,6 +31,11 @@ def split_data_sequence_parallel(data, label):
         and gpc.config.parallel["tensor"].get("mode", TensorParallelMode.mtp.name) == TensorParallelMode.isp.name
     ):
         data["indexes"] = _split(data["indexes"], ParallelMode.TENSOR, dim=_indexes_seq_dim)
+    if (
+        "position_ids" in data
+        and gpc.config.parallel["tensor"].get("mode", TensorParallelMode.mtp.name) == TensorParallelMode.isp.name
+    ):
+        data["position_ids"] = _split(data["position_ids"], ParallelMode.TENSOR, dim=_indexes_seq_dim)
 
     data["input_ids"] = _split(data["input_ids"], ParallelMode.TENSOR, dim=_seq_dim)
     label = _split(label, ParallelMode.TENSOR, dim=_seq_dim)
