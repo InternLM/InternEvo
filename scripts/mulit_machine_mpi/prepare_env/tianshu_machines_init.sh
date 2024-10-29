42a1cde521b61468fd5e8dcecc98dc121e8211cf
zhhsplendid <zhhsplendid@163.com>
2024-10-29 19:14:22 +0800

    Fix a bug

--- scripts/mulit_machine_mpi/prepare_env/tianshu_machines_init.sh
+++ scripts/mulit_machine_mpi/prepare_env/tianshu_machines_init.sh
@@ -1,9 +1,9 @@
 set -ex
 
 # One Machine TEST
-export HOST_FILE="../host_machines/host_machines_1node_tianshu.txt"
+# export HOST_FILE="../host_machines/host_machines_1node_tianshu.txt"
 
-# export HOST_FILE="../host_machines/host_machines_tianshu.txt"
+export HOST_FILE="../host_machines/host_machines_tianshu.txt"
 export NNODES=$(sed -n '=' $HOST_FILE | wc -l)
 
 export CUR_PATH=$(pwd)
@@ -14,12 +14,12 @@ cat $HOST_FILE
    docker load -i /share/work/zhenghuihuang/lumina/docker_images/tianshu_image.tar  
 
 /opt/maca/ompi/bin/mpirun -hostfile ${HOST_FILE} -np ${NNODES} \
-   docker run -it \
+   docker run -id \
      -v /usr/src:/usr/src \
      -v /lib/modules:/lib/modules \
      -v /dev:/dev \
      -v /share:/share \
-     -v /data:/data \ 
+     -v /data:/data \
      --name lumina_train_share \
      --network host \
      --privileged \
@@ -27,7 +27,7 @@ cat $HOST_FILE
      --pid=host \
      --group-add=video \
      -p 12000-13000:12000-13000 \
-     bi150-py310-torch2.1-ubuntu20.04:v4.1.1
+     bi150-py310-torch2.1-ubuntu20.04:v4.1.1 /bin/bash
 
 /opt/maca/ompi/bin/mpirun -hostfile ${HOST_FILE} -np ${NNODES} \
    docker exec lumina_train_share /bin/bash -c \
