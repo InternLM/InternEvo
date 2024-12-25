7b29800f772b9baddd5aaf6d4a82cd728ab842ed
lijiaxing <li126com2@126.com>
2024-12-25 15:37:43 +0800

    fix ci

--- internlm/initialize/launch.py
+++ internlm/initialize/launch.py
@@ -259,7 +259,7 @@ def args_sanity_check():
         # If 'auto_resume' is not given, we set it to True, so internlm can have opportunity
         # to auto-load latest checkpoint.
         ckpt._add_item("auto_resume", True)
-    
+
     if "universal_ckpt" not in ckpt:
         ckpt._add_item("universal_ckpt", dict(enable=False, aysnc_save=False, broadcast_load=False))
 
